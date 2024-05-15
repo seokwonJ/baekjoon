ba5144aaac97a04328c5a134d7620b5c5aaa95c3
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-05-15 16:34:26 +0900

    [Bronze III] Title: K 2K 게임, Time: 8 ms, Memory: 2412 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{	
+	vector <int> a;
+	long long int n, k;
+	cin >> n >> k;
+	for (long long int i = 1; i <= n; i++) {
+		if ((i % 10) != (k % 10) && (i % 10) != 2 * (k % 10)) {
+			a.push_back(i);
+		}
+	}
+
+	cout << a.size() << '\n';
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << ' ';
+	}
+	return 0;
+}
