dfb6656a993ac13b22a1584bbb2d99c6b9fcd8f2
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-06-19 23:06:36 +0900

    [Bronze V] Title: A-B, Time: 64 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,9 @@
+using System;
+namespace baejoon {
+    class minus {
+        static void Main(string[] args) {
+            string[] a = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(a[0]) - int.Parse(a[1]));
+        }
+    }
+}
