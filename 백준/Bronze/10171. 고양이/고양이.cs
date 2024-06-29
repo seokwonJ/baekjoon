0638ac8a5fc7889c91e28fc8d5503e8a6a056c95
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-06-29 14:38:12 +0900

    [Bronze V] Title: 고양이, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
