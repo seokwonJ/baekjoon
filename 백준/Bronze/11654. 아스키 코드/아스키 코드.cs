a94b89ee15a65e6985bc873bc09335d13932f76f
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-07-05 09:17:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics.PerformanceData;
+using System.Runtime.InteropServices;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            string s = Console.ReadLine();
+            Console.WriteLine(s[0] + 0);
+        }
+    }
+}
