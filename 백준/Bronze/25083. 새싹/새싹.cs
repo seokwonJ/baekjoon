2fe063ed9e6a72d016a4f884c00f89b9636ed91f
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-07-08 09:14:45 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics.PerformanceData;
+using System.Runtime.InteropServices;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}        
