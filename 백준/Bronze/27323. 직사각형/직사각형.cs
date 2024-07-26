9dba3d15ac0de8f1314080bf35bd61d71adf1d40
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-07-26 09:13:24 +0900

    [Bronze V] Title: 직사각형, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Runtime.InteropServices;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}        
