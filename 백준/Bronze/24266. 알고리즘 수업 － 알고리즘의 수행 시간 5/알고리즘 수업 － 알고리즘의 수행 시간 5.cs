d3fe3c98ad527f95c538f8d04b9bd49559f62d5f
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-08-08 09:14:57 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n * n * n);
+            Console.WriteLine(3);
+        }
+    }
+}
