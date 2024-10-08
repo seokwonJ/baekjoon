8027e0c6010cb4584c793cb6350715981df45947
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-10-08 12:50:29 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+    int answer = 0;
+    for (int i = 0;i < number.size(); i++) {
+        answer += number[i] - '0';
+    }
+    answer %= 9;
+    return answer;
+}
