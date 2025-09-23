fa38dcbbc9f574980aa0f3132b0645bddb9f11a4
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-09-23 13:52:54 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.95 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cs
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cs
@@ -1,27 +1,19 @@
 using System;
-
+using System.Linq;
 public class Solution {
-    public int solution(int[] sides) {
-        int answer = 0;
-        int max = 0;
-        int min =0;
-        if (sides[0] > sides[1])  {
-            max = sides[0];
-            min = sides[1];
+    public int solution(int[] sides)
+        {
+            int answer = 0;
+            int max = sides.Max();
+            int min = sides.Min();
+
+            for (int i = 1; i < max + min; i++)
+            {
+                if (max < i + min)
+                {
+                    answer++;
+                }
+            }
+            return answer;
         }
-        else {
-            max = sides[1];
-            min = sides[0];
-        } 
-        
-        for (int i =1;i < max;i++) {
-            if(min + i > max) answer++;
-        }
-        
-        for (int i =max;i < int.MaxValue;i++) {
-            if(min + max > i) answer++;
-            else break;
-        }
-        return answer;
-    }
 }
