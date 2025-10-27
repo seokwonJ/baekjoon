2f0cfc6f21535db26c83f440710507d60611ad91
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-10-27 14:54:52 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.34 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -3,8 +3,8 @@ using System;
 public class Solution {
     public int solution(string str1, string str2) {
         int answer = 0;
-        if (str1.Contains(str2)) answer = 1;
-        else answer = 2;
+        answer = str1.Contains(str2) == true ? 1 : 2;
+        
         return answer;
     }
 }
