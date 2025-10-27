c8d64286331b5533b01196a3a706e2c79a8eaead
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-10-27 15:45:42 +0900

    [level 0] Title: 배열의 유사도, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(string[] s1, string[] s2) {
+        int answer = 0;
+        foreach(string str1 in s1) {
+            foreach (string str2 in s2) {
+                if (str1 == str2) answer++;
+            }
+        }
+        return answer;
+    }
+}
