526c2e4a3e837eaf01fdcf36ad74e2c75d25e021
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-11-04 16:21:37 +0900

    [level 0] Title: 숫자 찾기, Time: 3.42 ms, Memory: 31.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cs
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cs
@@ -3,17 +3,8 @@ using System;
 public class Solution {
     public int solution(int num, int k) {
         int answer = 0;
-        string s = num.ToString();
-        int index = 0;
-        for (int i = 0;i < s.Length;i++) {
-            if (s[i] == '0' + k ) {
-                index = i+1;
-                break;
-            }
-        }
-        
-        if (index == 0) answer = -1;
-        else answer = index;
+        int index = num.ToString().IndexOf(k.ToString()) + 1;
+        answer = index == 0 ? -1 : index;
         
         return answer;
     }
