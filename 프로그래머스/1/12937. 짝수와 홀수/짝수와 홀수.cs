93cf2d7a34c8f7b305dae6f3517e6ce3a5705880
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-11-27 13:22:21 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cs
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -1,8 +1,5 @@
 public class Solution {
     public string solution(int num) {
-        string answer = "";
-        if (num % 2 == 1 || num % 2 == -1) return "Odd";
-        else if(num % 2 == 0) return "Even";
-        else return "NO";
+        return (num % 2 == 0) ? "Even" : "Odd";
     }
 }
