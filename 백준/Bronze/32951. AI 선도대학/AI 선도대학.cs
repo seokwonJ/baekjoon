dfa2df338fdb7ca943b1a881290752feb709d642
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-04 22:12:22 +0900

    [Bronze V] Title: AI 선도대학, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(a - 2024);
+        }
+    }
+}
