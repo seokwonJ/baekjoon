05469018648cc5e0d40549ee013e07134a5b9bd5
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-05 09:49:28 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace hello {
+    class hi {
+        static void Main(string[] args) {
+            string s1; 
+            s1 = Console.ReadLine();
+            string s2;
+            s2 = Console.ReadLine();
+            if (s1 == s2) Console.WriteLine(0);
+            else Console.WriteLine(1550);
+        }
+    }
+}
