72576576810ead8b8201de1523074ab1cdb996da
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-05 15:16:16 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 76 ms, Memory: 11940 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace hello{
+    class h {
+        static void Main(string[] args) {
+            int num = int.Parse(Console.ReadLine());
+            for (int i=0;i < num;i++) {
+                int num1 = int.Parse(Console.ReadLine());
+                for (int j=0;j < num1+1;j++) {
+                    string s = Console.ReadLine();
+                }
+            }
+            
+            for (int i=0;i < num;i++) {
+                Console.WriteLine("Material Management " + (i+1));
+                Console.WriteLine("Classification ---- End!");
+            }
+        }
+    }
+}
