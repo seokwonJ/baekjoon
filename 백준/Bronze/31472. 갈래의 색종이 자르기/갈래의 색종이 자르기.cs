f04b8307336d5c0c8eb5a587697af77e5b0d129d
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-08 13:43:25 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace hello {
+    class h {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((Math.Sqrt(n*2)) * 4);
+        }
+    }
+}
