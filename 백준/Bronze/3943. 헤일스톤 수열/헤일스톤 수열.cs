4cff7046410471b24a0336adc5012e47d9f9d35e
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-08 16:59:57 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 180 ms, Memory: 15336 KB -BaekjoonHub

--- 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.cs
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.cs
@@ -1,37 +1,23 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace hello {
     class h {
         static void Main(string[] args) {
             int n = int.Parse(Console.ReadLine());
-            Dictionary<long,long> di = new Dictionary<long,long>();
-            
             StringBuilder sb = new StringBuilder();
-            
             for (int i =0;i < n;i++) {
-                long nums = long.Parse(Console.ReadLine());
-                long num = nums;
+                long num = long.Parse(Console.ReadLine());
                 long max = 0;
-
                 while (true) {
-                    if (di.ContainsKey(num)){
-                        if(di[num] > max) {
-                            di[nums] = di[num];
-                            max = di[num];
-                        }
-                        else di[nums] = max;
-                        break;
-                    }
                     if (num > max) max = num;
                     if (num == 1) break;
                     else if (num % 2 == 0) num /= 2;
-                    else num = num * 3 + 1;
+                    else if (num % 2 == 1) num = num * 3 + 1;
                 }
                 sb.AppendLine(max.ToString());
             }
-                Console.WriteLine(sb.ToString());
+                            Console.WriteLine(sb.ToString());
         }
     }
 }
