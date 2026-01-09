2eaaaa471880d23cea1557812ce5490beda15e19
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-09 15:26:56 +0900

    [Bronze II] Title: 스페셜 저지, Time: 420 ms, Memory: 6468 KB -BaekjoonHub

--- 백준/Bronze/9253. 스페셜 저지/스페셜 저지.cs
+++ 백준/Bronze/9253. 스페셜 저지/스페셜 저지.cs
@@ -6,11 +6,7 @@ namespace hello {
             string s1 = Console.ReadLine();
             string s2 = Console.ReadLine();
             string s3 = Console.ReadLine();
-            bool isTrue = false;
-            bool isTrue2 = false;
-            if (s1.Contains(s3)) isTrue = true;
-            if (s2.Contains(s3)) isTrue2 = true;
-            if (isTrue && isTrue2) Console.WriteLine("YES");
+            if (s1.Contains(s3) && s2.Contains(s3)) Console.WriteLine("YES");
             else Console.WriteLine("NO");
         }
     }
