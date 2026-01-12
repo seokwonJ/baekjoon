e23413ac7e596fe428ef5a188eac666579d612ab
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-13 03:28:45 +0900

    [Silver V] Title: FIFA 월드컵, Time: 48 ms, Memory: 5676 KB -BaekjoonHub

--- 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.cs
+++ 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.cs
@@ -13,15 +13,16 @@ namespace hello
                 if (s == "-1 -1 -1 -1") break;
                 string[] ss = s.Split();
                 long[] num = Array.ConvertAll(ss, long.Parse);
-                double personNow = num[0] * num[2] + num[3];
-                double forPersonNow = 0;
-                double totalPlayCount = 1;
-                totalPlayCount *= num[1] * (num[1] - 1) / 2;
+                long personNow = num[0] * num[2] + num[3];
+                long forPersonNow = 0;
+                long totalPlayCount = 1;
+
+                totalPlayCount = num[1] * (num[1]-1)/2;
                 totalPlayCount *= num[0];
 
                 double powNum = Math.Pow(2, 65);
 
-                for (double i = 1; i <= powNum; i *= 2)
+                for (long i = 1; i <= powNum; i *= 2)
                 {
                     if (i >= personNow)
                     {
@@ -29,7 +30,7 @@ namespace hello
                         break;
                     }
                 }
-                for (double i = forPersonNow / 2; i >= 1; i /= 2)
+                for (long i = forPersonNow / 2; i >= 1; i /= 2)
                 {
                     totalPlayCount += i;
                 }
