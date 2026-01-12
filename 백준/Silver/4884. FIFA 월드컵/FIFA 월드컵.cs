54ba302f63e4388a7b9c3ba23b38633490557cf5
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2026-01-13 03:15:55 +0900

    [Silver V] Title: FIFA 월드컵, Time: 52 ms, Memory: 5700 KB -BaekjoonHub

--- 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.cs
+++ 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.cs
@@ -6,49 +6,38 @@ namespace hello
     {
         static void Main(string[] args)
         {
+
             while (true)
             {
                 string s = Console.ReadLine();
-                if (s == null || s == "-1 -1 -1 -1") break; // null 체크 추가 권장
-
+                if (s == "-1 -1 -1 -1") break;
                 string[] ss = s.Split();
-                // 입력 숫자가 클 수 있으므로 long으로 변환
                 long[] num = Array.ConvertAll(ss, long.Parse);
+                long personNow = num[0] * num[2] + num[3];
+                long forPersonNow = 0;
+                long totalPlayCount = 1;
+                totalPlayCount *= num[1] * (num[1] - 1) / 2;
+                totalPlayCount *= num[0];
 
-                long G = num[0];
-                long T = num[1];
-                long A = num[2];
-                long D = num[3];
+                double powNum = Math.Pow(2, 65);
 
-                // 1. 조별 예선 경기 수 구하기 (공식 사용)
-                // G * (T * (T-1) / 2)
-                long groupMatches = G * (T * (T - 1) / 2);
-
-                // 2. 토너먼트 진출 팀 수
-                long P = G * A + D;
-
-                // 3. 2의 제곱수 맞추기
-                long powerOf2 = 1;
-                while (powerOf2 < P)
+                for (long i = 1; i <= powNum; i *= 2)
                 {
-                    powerOf2 *= 2;
+                    if (i >= personNow)
+                    {
+                        forPersonNow = i;
+                        break;
+                    }
                 }
 
-                // 4. 추가해야 할 팀(Y)과 토너먼트 경기 수 계산
-                long Y = powerOf2 - P;
-                
-                // 토너먼트 총 경기 수는 (참가팀 수 - 1)입니다. 
-                // 예: 8강 -> 7경기, 4강 -> 3경기
-                long tournamentMatches = 0;
-                if (powerOf2 > 0) 
+                for (long i = forPersonNow / 2; i >= 1; i /= 2)
                 {
-                    tournamentMatches = powerOf2 - 1;
+                    totalPlayCount += i;
                 }
 
-                long X = groupMatches + tournamentMatches;
+                forPersonNow = forPersonNow - personNow;
 
-                // 출력 형식: G*A/T+D=X+Y
-                Console.WriteLine($"{G}*{A}/{T}+{D}={X}+{Y}");
+                Console.WriteLine(num[0] + "*" + num[2] + "/" + num[1] + "+" + num[3] + "=" + totalPlayCount + "+" + forPersonNow);
             }
         }
     }
